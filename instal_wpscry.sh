2a1562f09183bbaa0f34d387e8eed5a15769b3bf
bgropay <bgropay@gmail.com>
2024-08-10 14:27:31 +0700

    Add files via upload

--- instal_wpscry.sh
+++ instal_wpscry.sh
@@ -13,7 +13,8 @@ error=()
 r="\e[0m"
 m="\e[1;31m"
 h="\e[1;32m"
-b="\e[1;34m"
+c="\e[1;34m"
+b="\e[1;36m"
 p="\e[1;37m"
 
 # Fungsi untuk memeriksa apakah script dijalankan sebagai root.
@@ -26,9 +27,9 @@ function cek_root(){
 
 function konfirmasi(){
         clear
-        echo "--------------------------------------------------------------------"
-        echo "Script ini merupakan script untuk menginstal wpscry secara otomatis."
-        echo "--------------------------------------------------------------------" 
+        echo -e "${p}--------------------------------------------------------------------${r}"
+        echo -e "${c}Script ini merupakan script untuk menginstal wpscry secara otomatis.${r}"
+        echo -e "${p}--------------------------------------------------------------------${r}" 
         echo ""
         while true; do
                 read -p "Apakah Anda ingin menginstal wpscry (iya/tidak): " nanya
@@ -190,43 +191,43 @@ function instal_pixiewps(){
         # done
 
         # Instal pixiewps
-        echo "[*] Mengkloning pixiewps dari github..."
+        echo -e "${b}[*] ${p}Mengkloning pixiewps dari github...${r}"
         sleep 3
         git clone "${url_pixiewps}" >> /dev/null
 
         if [[ $? -eq 0 ]]; then
-                echo "[+] pixiewps berhasil dikloning dari github."
+                echo -e "${h}[+] ${p}pixiewps berhasil dikloning dari github.${r}"
                 sleep 3
         else
-                echo "[-] pixiewps gagal dikloning dari github."
+                echo -e "${m}[-] ${p}pixiewps gagal dikloning dari github.${r}"
 		((error+=1))
                 sleep 3
         fi
 
         cd "${path_pixiewps}"
 
-        echo "[*] Mengkompilasi pixiewps..."
+        echo -e "${b}[*] ${p}Mengkompilasi pixiewps...${r}"
         sleep 3
         make >> /dev/null
 
         if [[ $? -eq 0 ]]; then
-                echo "[+] pixiewps berhasil dikompilasi."
+                echo -e "${h}[+] ${p}pixiewps berhasil dikompilasi.${r}"
                 sleep 3
         else
-                echo "[-] pixiewps gagal dikompilasi."
+                echo -e "${m}[-] ${p}pixiewps gagal dikompilasi.${r}"
 		((error+=1))
                 sleep 3
         fi
 
-        echo "[*] Menginstal pixiewps..."
+        echo -e "${b}[*] ${p}Menginstal pixiewps...${r}"
         sleep 3
         make install >> /dev/null
 
         if [[ $? -eq 0 ]]; then
-                echo "[+] pixiewps berhasil diinstal."
+                echo -e "${h}[+] ${p}pixiewps berhasil diinstal.${r}"
                 sleep 3
         else
-                echo "[-] pixiewps gagal diinstal."
+                echo -e "${m}[-] ${p}pixiewps gagal diinstal.${r}"
 		((error+=1))
                 sleep 3
         fi
@@ -269,96 +270,96 @@ function instal_aircrack(){
 
         # Menginstal seluruh dependensi yang diperlukan oleh aircrack-ng
         for dependensi_aircrack in "${daftar_dependensi_aircrack[@]}"; do
-                echo "[*] Menginstal dependensi '${dependensi_aircrack}'..."
+                echo -e "${b}[*] ${p}Menginstal dependensi '${dependensi_aircrack}'...${r}"
                 sleep 3
                 apt-get install "${dependensi_aircrack}" -y >> /dev/null
                 if [[ $? -eq 0 ]]; then
-                        echo "[+] Dependensi '${dependensi_aircrack}' berhasil diinstal."
+                        echo -e "${h}[+] ${p}Dependensi '${dependensi_aircrack}' berhasil diinstal.${r}"
                         sleep 3
                 else
-                        echo "[-] Dependensi '${dependensi_aircrack}' gagal diinstal."
+                        echo -e "${m}[-] ${p}Dependensi '${dependensi_aircrack}' gagal diinstal.${r}"
 			((error+=1))
                         sleep 3
                 fi
         done
 
         # Instal aircrack-ng
-        echo "[*] Mengkloning aircrack-ng dari github..."
+        echo -e "${b}[*] ${p}Mengkloning aircrack-ng dari github...${r}"
         sleep 3
         git clone "${url_aircrack}" >> /dev/null
 
         if [[ $? -eq 0 ]]; then
-                echo "[+] aircrack-ng berhasil dikloning dari github."
+                echo -e "${h}[+] ${p}aircrack-ng berhasil dikloning dari github.${r}"
                 sleep 3
         else
-                echo "[-] aircrack-ng gagal dikloning dari github."
+                echo -e "${m}[-] ${p}aircrack-ng gagal dikloning dari github.${r}"
 		((error+=1))
                 sleep 3
         fi
 
         cd "${path_aircrack}"
 
-        echo "[*] Menghasilkan file configure..."
+        echo -e "${b}[*] ${p}Menghasilkan file configure...${r}"
         sleep 3
         autoreconf -i >> /dev/null
 
         if [[ $? -eq 0 ]]; then
-                echo "[+] File configure berhasil dihasilkan."
+                echo -e "${h}[+] ${p}File configure berhasil dihasilkan.${r}"
                 sleep 3
         else
-                echo "[-] File configure gagal dihasilkan."
+                echo -e "${m}[-] ${p}File configure gagal dihasilkan.${r}"
 		((error+=1))
                 sleep 3
         fi
 
-        echo "[*] Menghasilkan file Makefile..."
+        echo -e "${b}[*]${p} Menghasilkan file Makefile...${r}"
         sleep 3
         ./configure --with-experimental >> /dev/null
 
         if [[ $? -eq 0 ]]; then
-                echo "[+] File Makefile berhasil dihasilkan."
+                echo -e "${h}[+] ${p}File Makefile berhasil dihasilkan.${r}"
                 sleep 3
         else
-                echo "[-] File Makefile gagal dihasilkan."
+                echo -e "${m}[-] ${p}File Makefile gagal dihasilkan.${r}"
 		((error+=1))
                 sleep 3
         fi
 
-        echo "[*] Mengkompilasi aircrack-ng..."
+        echo -e "${b}[*] ${p}Mengkompilasi aircrack-ng...${r}"
         sleep 3
         make >> /dev/null
 
         if [[ $? -eq 0 ]]; then
-                echo "[+] aircrack-ng berhasil dikompilasi."
+                echo -e "${h}[+] ${p}aircrack-ng berhasil dikompilasi.${r}"
                 sleep 3
         else
-                echo "[-] aircrack-ng gagal dikompilasi."
+                echo -e "${m}[-] ${p}aircrack-ng gagal dikompilasi.${r}"
 		((error+=1))
                 sleep 3
         fi
 
-        echo "[*] Menginstal aircrack-ng..."
+        echo -e "${b}[*] ${p}Menginstal aircrack-ng...${r}"
         sleep 3
         make install >> /dev/null
 
         if [[ $? -eq 0 ]]; then
-                echo "[+] aircrack-ng berhasil diinstal."
+                echo -e "${h}[+] ${p}aircrack-ng berhasil diinstal.${r}"
                 sleep 3
         else
-                echo "[-] aircrack-ng gagal diinstal."
+                echo -e "${m}[-] ${p}aircrack-ng gagal diinstal.${r}"
 		((error+=1))
                 sleep 3
         fi
 
-        echo "[*] Mengatur path library..."
+        echo -e "${b}[*] ${p}Mengatur path library...${r}"
         sleep 3
         ldconfig >> /dev/null
 
         if [[ $? -eq 0 ]]; then
-                echo "[+] Path library berhasil diatur."
+                echo -e "${h}[+] ${p}Path library berhasil diatur.${r}"
                 sleep 3
         else
-                echo "[-] Path library gagal diatur."
+                echo -e "${m}[-] ${p}Path library gagal diatur.${r}"
 		((error+=1))
                 sleep 3
         fi
@@ -366,20 +367,26 @@ function instal_aircrack(){
         cd ../ # kembali ke direktori '/opt'
 }
 
+function tambahkan_ke_path(){
+	file="src/wpscry"
+	chmod +x "${file}"
+	cp "${file}" /usr/bin
+}
+
 function cek_error(){
         if [[ "${#error[@]}" -ne 0 ]]; then
-	        echo "--------------------------------------------------------------------"
-                echo "[-] wpscry gagal diinstal."
-		echo "--------------------------------------------------------------------"
+	        echo -e "${p}--------------------------------------------------------------------${r}"
+                echo -e "${m}[-] ${p}wpscry gagal diinstal.${r}"
+		echo -e "${p}--------------------------------------------------------------------${r}"
 	        sleep 3
 	        exit 1
 	else
-                echo "--------------------------------------------------------------------"
-                echo "[+] wpscry berhasil diinstal."
-		sleep3
-                echo "[+] Ketikkan 'wpscry' untuk menjalankannya."
+                echo -e "${p}--------------------------------------------------------------------${r}"
+                echo -e "${h}[+] ${p}wpscry berhasil diinstal.${r}"
 		sleep 3
-		echo "--------------------------------------------------------------------"
+                echo -e "${h}[+] ${p}Ketikkan 'wpscry' untuk menjalankannya.${r}"
+		sleep 3
+		echo -e "${p}--------------------------------------------------------------------${r}"
 	        sleep 3
 	        exit 0
 	fi
@@ -396,6 +403,7 @@ function instal_wpscry(){
         instal_reaver
 	instal_pixiewps
         instal_aircrack
+	tambahkan_ke_path
 	cek_error
 }
 
